b3c4ff259ad588d95d8fa22cb672ef2561d90f80
Garrett Leung <garrettleung91@gmail.com>
2021-08-05 12:27:17 -0700

    export pages

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+export { default as Navigation } from "./components/Navigation";
+export { default as Footer } from "./components/Footer";
+export { default as Home } from "./components/Home";
+export { default as Projects } from "./components/Projects";
+export { default as Resume } from "./components/Resume";
+export { default as Yelp } from "./components/Yelp";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
