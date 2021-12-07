89668decc32780de62e53a945d13592866188969
Garrett Leung <garrettleung91@gmail.com>
2021-12-06 19:54:16 -0800

    set up resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,8 +6,10 @@ function Resume() {
       <div class="container">
         <div class="row align-items-center my-5">
 
-          <div class="resume-space"> </div>
-          <iframe margin-top = "2%" width = "100%" height = "1000px" src="https://docs.google.com/document/d/e/2PACX-1vSOJ6w85P22WXRlUdg6o2GdwordAb_5J1hsPvl5fmPOlwTOASgMx4z8PlA3xZ_YI52oXDV5nV6GxoAp/pub?embedded=true"></iframe>
+          <div class="resume-space">
+            <iframe width = "850px" height = "1200px" src="https://docs.google.com/document/d/e/2PACX-1vSOJ6w85P22WXRlUdg6o2GdwordAb_5J1hsPvl5fmPOlwTOASgMx4z8PlA3xZ_YI52oXDV5nV6GxoAp/pub?embedded=true"></iframe> 
+          </div>
+      
         </div>
       </div>
     </div>
